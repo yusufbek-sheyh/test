16b5bf0829d1c52650d5a0c25628af51698e782d
yusufbek-sheyh <yusufbek190991@gmail.com>
2022-02-22 21:14:59 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var info = alert("Assalom aleykum .!")
+var info = alert("Assalom aleykum .! najot talim oquv markaziga huw kelibsiz")
+var isName = prompt("Ismingiz", "yusufbek");
+
+var surName = prompt("Familiyangiz", "Nuriddinov");
+var fatherName = prompt("ochestva", "....");
+var hobby = prompt("hobiyiz "  , " football")
+var isAge = prompt("Yoshingiz", "15");
+var isAdress = prompt("adres", "Tashkent");
+var areStudent = prompt("oquvchimisiz ?", "Ha");
+var isMarried = prompt("marriend ?", "Yo'q");
+var telNumber = prompt("Telefon raqamingiz", "+9989");
+
+
+
+
+
+
+console.log("isName" + "surName");
+
+var result = "Assalomu aleykum " + isName + " " + surName + " " + fatherName + " "  + hobby + " " +  + isAge + " " + isAdress + " " + areStudent + " " +  isMarried + " " +  telNumber  
+
+console.log(result);
+
+
+
+alert(result)
